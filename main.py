709922745902f46e22bde0d6fe1c05c6dda8461d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-22 02:29:03 +0400

    21.10. Задача "Координатные четверти"

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-# Вариант 1
-n, k = int(input()), int(input())
-res = 0
-for i in range(1, n + 1):
-    res = (res + k) % i
-print(res + 1)
+n = int(input())
+count = [0, 0, 0, 0]
+names = ['Первая четверть:', 'Вторая четверть:',
+         'Третья четверть:', 'Четвертая четверть:']
 
-# Вариант 2
-n, k = int(input()), int(input())
-res = []
-for i in range(1, n + 1):
-    res.append(i)
-index = 0
-while len(res) > 1:
-    index = (index + k - 1) % len(res)      # новый_индекс = (текущий_индекс + k - 1) % len(список)
-    removed = res.pop(index)            # k - 1 потому что, если мы на 1 человеке, а хотим убить 3, нам нужно сделать 2 шага (3 - 1)
-print(*res)
+for _ in range(n):
+    x, y = [int(num) for num in input().split()]
+    if x > 0 and y > 0:
+        count[0] += 1
+    elif x < 0 and y > 0:
+        count[1] += 1
+    elif x < 0 and y < 0:
+        count[2] += 1
+    elif x > 0 and y < 0:
+        count[3] += 1
+
+for i in range(4):
+    print(names[i], count[i])
