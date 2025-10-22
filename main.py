d9ec13e3b9bb7384188380c543eb144544fa2392
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-23 02:28:16 +0400

    22.10. Задача "Сдвиг в развитии"

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+#Вариант 1
 n = [int(x) for x in input().split()]
 
-for i in range(0, len(n) - 1, 2):
-    n[i], n[i + 1] = n[i + 1], n[i]
+for i in range(1, len(n)):
+    n[0], n[i] = n[i], n[0]
 print(*n)
+
+#Вариант 2
+n = [int(x) for x in input().split()]
+
+for i in range(len(n) - 1, 0, -1 ):
+    n[i], n[i - 1] = n[i - 1], n[i]
+print(*n)
+
+#Вариант 3
+seq = input().split()
+new_seq = [seq[-1]] + seq[:-1]
+
+print(*new_seq)
