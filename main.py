b2d000a1b1bbc13cd4e05685e90b2353cb67e0a4
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-23 13:35:02 +0400

    23.10. Задача "Различные элементы"

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-#Вариант 1
 n = [int(x) for x in input().split()]
-
+count = 1
 for i in range(1, len(n)):
-    n[0], n[i] = n[i], n[0]
-print(*n)
-
-#Вариант 2
-n = [int(x) for x in input().split()]
-
-for i in range(len(n) - 1, 0, -1 ):
-    n[i], n[i - 1] = n[i - 1], n[i]
-print(*n)
-
-#Вариант 3
-seq = input().split()
-new_seq = [seq[-1]] + seq[:-1]
-
-print(*new_seq)
+    if n[i] != n[i - 1]:
+        count += 1
+print(count)
