8fc6966eeecdb18afafa71d0ef8e144836dc9327
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-30 13:58:57 +0400

    30.10. Треугольник Паскаля 2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+def pascal(line):
+    li = [1]
+    for i in range(line):
+        for j in range(len(li) - 1):
+            li[j] = li[j] + li[j + 1]
+        li.insert(0, 1)          # Добавляем 1 в начало
+    return li
 n = int(input())
-
-li = [1]
-for _ in range(n):
-    for j in range(len(li) - 1):
-        li[j] = li[j] + li[j + 1]
-    li.insert(0, 1)          # Добавляем 1 в начало
-
-print(li)
+for element in range(n):
+    print(*pascal(element))
