461b2a8c352303bb90ed5bf7f3060af9a6dad049
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-03 01:13:38 +0400

    02.11. Больше среднего

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# 1
 n = int(input())
 matrix = []
-count = 0
 for i in range(n):
     temp = [int(num) for num in input().split()]
     matrix.append(temp)
-    count += matrix[i][i]
-print(count)
+    average = sum(temp) / len(temp)                     # sum(temp) - сумма всех элементов строки, len(temp) - количество элементов в строке
+    count = 0
+    for j in temp:
+        if j > average:
+            count += 1
+    print(count)
 
 
-# 2
-res = 0
-for i in range(int(input())):
-    res += int(input().split()[i])
-print(res)
+
