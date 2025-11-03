c4cb09850d28813a44b67957be97286eaff2529d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-03 20:15:16 +0400

    03.11. Таблица умножения.

--- main.py
+++ main.py
@@ -1,24 +1,2 @@
-n = int(input())
-matrix = []
-
-for _ in range(n):
-    row = [int(i) for i in input().split()]
-    matrix.append(row)
-
-up, right, down, left = 0, 0, 0, 0
-
-for i in range(n):
-    for j in range(n):
-        if i < j and i < n - 1 - j:
-            up += matrix[i][j]
-        elif i < j and i > n - 1 - j:
-            right += matrix[i][j]
-        elif i > j and i > n - 1 - j:
-            down += matrix[i][j]
-        elif i > j and i < n - 1 - j:
-            left += matrix[i][j]
-
-print("Верхняя четверть:", up)
-print("Правая четверть:", right)
-print("Нижняя четверть:", down)
-print("Левая четверть:", left)
+n, m = int(input()), int(input())
+mult = [print(*[str(i*j).ljust(3) for j in range(m)]) for i in range(n)]
