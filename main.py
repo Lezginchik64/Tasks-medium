bd5196cff201e2c1d11b2c30807751db0a2af30a
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-03 16:48:00 +0400

    03.11. Максимальный в области 2 🌶️

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ for _ in range(n):
     row = [int(i) for i in input().split()]
     matrix.append(row)
 
-largest = matrix[0][0]
-for i in range(n):
-    for j in range(n):
-        if i >= j and matrix[i][j] > largest:      # сравниваем элементы главной диагоналии всего нижнего левого угла, начиная с largest
-            largest = matrix[i][j]
+largest = matrix[0][0]                              # i >= j → значит под главной диагональю (включая её)
+                                                    # i + j + 1 <= n → значит над побочной диагональю (включая её)
+for i in range(n):                                  # i <= j → значит над главной диагональю (включая её).
+    for j in range(n):                              # i + j + 1 >= n → значит под побочной диагональю (включая её).
+        if (i >= j and i + j + 1 <= n) or (i <= j and i + j + 1 >= n):
+            if matrix[i][j] > largest:
+                largest = matrix[i][j]
 print(largest)
