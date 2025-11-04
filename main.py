d9eba6c2328a9b2a34195355b784d8a041770871
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-05 01:03:57 +0400

    04.11. Зеркальное отображение 🦋

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+# 1
 n = int(input())
 matrix = [input().split() for _ in range(n)]
 
-for i in range(n):
-    matrix[i][i], matrix[n - i - 1][i] = matrix[n - i - 1][i], matrix[i][i]   # matrix[n - i - 1][i] - обмен главной и диагонали, отражённой относительно горизонтальной оси.
-                                # matrix[i][n - i - 1] - обмен главной и диагонали, отражённой относительно вертикальной оси.
+for row in reversed(matrix):
+    print(*row)
+
+# 2
+n = int(input())
+matrix = [input().split() for _ in range(n)]
+
+for i in range(n // 2):
+    matrix[i], matrix[n - i - 1] = matrix[n - i - 1], matrix[i]
+                            # matrix[n - i - 1] - строка снизу (с конца)
 for row in matrix:
     print(*row)
