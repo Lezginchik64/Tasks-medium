90aa01c25aa75206dcf875c27d79143d7f01e4df
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-05 01:35:20 +0400

    04.11. Поворот матрицы на 90 градусов 🔁

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# 1
-n = int(input())
-matrix = [input().split() for _ in range(n)]
-
-for row in reversed(matrix):
-    print(*row)
-
-# 2
 n = int(input())
 matrix = [input().split() for _ in range(n)]
 
 for i in range(n // 2):
-    matrix[i], matrix[n - i - 1] = matrix[n - i - 1], matrix[i]
+    matrix[i], matrix[n - i - 1] = matrix[n - i - 1], matrix[i]         # Переворачивает строки сверху вниз
                             # matrix[n - i - 1] - строка снизу (с конца)
-for row in matrix:
-    print(*row)
+for i in range(n):
+    for j in range(n):
+        print(matrix[j][i], end=" ")                # Транспонирование - меняет местами строки и столбцы.
+    print()                                         # То есть matrix[j][i] вместо matrix[i][j].
