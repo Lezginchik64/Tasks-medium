d7a4b453b6595747c20b4d48a5bed76021c15db4
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-05 00:49:26 +0400

    04.11. Обмен диагоналей 🔃

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 n = int(input())
 matrix = [input().split() for _ in range(n)]
-flag = True
 
 for i in range(n):
-    for j in range(n):
-        if matrix[i][j] != matrix[j][i]:
-            flag = False
-            break
-    if not flag:
-        break
-
-if flag:
-    print("YES")
-else:
-    print("NO")
+    matrix[i][i], matrix[n - i - 1][i] = matrix[n - i - 1][i], matrix[i][i]   # matrix[n - i - 1][i] - обмен главной и диагонали, отражённой относительно горизонтальной оси.
+                                # matrix[i][n - i - 1] - обмен главной и диагонали, отражённой относительно вертикальной оси.
+for row in matrix:
+    print(*row)
