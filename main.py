6818e726478ffc343621711d42418afbde2b742d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-07 13:19:35 +0400

    07.11. Заполнение 3

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-# 1
-n, m = [int(x) for x in input().split()]
-matrix = [[int(i) for i in range(m)] for _ in range(n)]
-count = 1
+n = int(input())
+matrix = [[0]*n for _ in range(n)]
 
-for j in range(m):
-    for i in range(n):
-        matrix[i][j] = count
-        count += 1
+for i in range(n):
+    matrix[i][i] = 1
+    matrix[i][n - i - 1] = 1
+
 
 for row in matrix:
     print(*[str(x).ljust(3) for x in row])
 
-# 2
-n, m = [int(x) for x in input().split()]
-matrix = [[j * n + i + 1 for j in range(m)] for i in range(n)]          # j * n + i + 1 — формула, номер = (сколько элементов в предыдущих столбцах) + (позиция внутри текущего столбца) + 1
-
-for row in matrix:
-    print(*[str(x).ljust(3) for x in row])
