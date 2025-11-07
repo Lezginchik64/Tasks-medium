2ce8dab868ce8650a2dbcefed438069ca0499875
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-07 14:10:05 +0400

    07.11. Заполнение 5 🌶️

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-# 1
-n = int(input())
-matrix = [[0] * n for _ in range(n)]                # i <= j — значит над или на главной диагонали
-                                                    # i >= j — значит под или на главной диагонали
-for i in range(n):                                  # На побочной диагонали всегда i + j + 1 == n.
-    for j in range(n):                              # Если меньше (< n) → это верхняя часть над побочной диагональю.
-        if (i <= j and i + j + 1 <= n) or (i >= j and i + j + 1 >= n):      # Если больше (> n) → это нижняя часть под ней.
-            matrix[i][j] = 1
+n, m = [int(i) for i in input().split()]
+matrix = [[int(i) for i in range(m)] for _ in range(n)]
 
-for row in matrix:
-    print(*[str(x).ljust(3) for x in row])
+for i in range(n):
+    for j in range(m):                              # i + j - создаёт линейный сдвиг по строкам
+        matrix[i][j] = (i + j) % m + 1              # % m - это цикличность, идет по кругу. % m + 1 - добавляем 1, чтобы получить диапазон от 1 до m
 
-# 2
-n = int(input())
-matrix = [[0] * n for _ in range(n)]
-
-for i in range(n):                                              # Побочная диагональ: i == n - 1 - j -> i + 1 + j == n
-    for j in range(n):
-        if i == j or i == n - 1 - j or (i < j and i < n - 1 - j) or (i > j and i > n - 1 - j):
-            matrix[i][j] = 1
-
-for row in matrix:
-    print(*[str(x).ljust(3) for x in row])
+for res in matrix:
+    print(*[str(i).ljust(3) for i in res])
