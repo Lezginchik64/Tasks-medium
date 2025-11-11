4529e8d2622736aec5c067e6452e6a8120c706ec
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-12 00:07:46 +0400

    11.11 Умножение матриц 🌶️

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-# 1
 n, m = [int(i) for i in input().split()]
 matrixA = [[int(i) for i in input().split()] for _ in range(n)]
 input()
-matrixB = [[int(i) for i in input().split()] for _ in range(n)]
-matrixC = [[0] * m for _ in range(n)]
+m, k = [int(i) for i in input().split()]
+matrixB = [[int(i) for i in input().split()] for _ in range(m)]
+matrixC = [[0] * k for _ in range(n)]
 
-for i in range(n):
-    for j in range(m):
-        matrixC[i][j] = matrixA[i][j] + matrixB[i][j]
+for i in range(n):              # идём по строкам первой матрицы, n - количество строк в первой матрице A.
+    for j in range(k):          # идём по столбцам второй матрицы, количество столбцов второй матрицы B.
+        for r in range(m):      # идём по элементам строки/столбца, m — это количество пар элементов, которые мы перемножаем. Количество столбцов A и строк B.
+            matrixC[i][j] += matrixA[i][r] * matrixB[r][j]
 
 for row in matrixC:
     print(*row)
-
-# 2
-n, m = [int(i) for i in input().split()]
-matrix_1 = [input().split() for i in range(n)]
-input()
-matrix_2 = [input().split()  for j in range(n)]
-matrix_3 = []
-
-for i in range(n):
-    row = []
-    for j in range(m):
-        matrix_3_el = int(matrix_1[i][j]) + int(matrix_2[i][j])
-        row.append(matrix_3_el)
-    matrix_3.append(row)
-
-for el in matrix_3:
-    print(*el)
