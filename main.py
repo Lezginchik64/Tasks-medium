e8a3f63c803b0d230967c4531ab3f07faa9882d7
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-12 00:17:44 +0400

    11.11 Возведение матрицы в степень 🌶 Способ 2. С функцией.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from copy import deepcopy
+def square_matrix_mult(matrixA, matrixB, size):
+    matrixC = [[0] * size for _ in range(size)]
+    for i in range(size):
+        for j in range(size):
+            for q in range(size):
+                matrixC[i][j] += matrixA[i][q] * matrixB[q][j]
+    return matrixC
+
 
 n = int(input())
 matrix = [[int(i) for i in input().split()] for _ in range(n)]
 m = int(input())
-matrix1 = deepcopy(matrix)  # создаем копию матрицы
+powered_matrix = matrix.copy()
 
-for k in range(m - 1):
-    matrix2 = [[0 for i in range(n)] for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            for r in range(n):
-                matrix2[i][j] += matrix1[i][r] * matrix[r][j]
-    matrix1 = deepcopy(matrix2)
+for _ in range(m - 1):
+    powered_matrix = square_matrix_mult(matrix, powered_matrix, n)
 
-for row in matrix1:
+for row in powered_matrix:
     print(*row)
