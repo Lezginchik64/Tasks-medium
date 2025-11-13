3c49ab871562b65bf99a43720548d77723bc0f08
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-14 03:35:35 +0400

    13.11 Сортировка и преобразование кортежей. Теория.

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from retinaface import RetinaFace
-import matplotlib.pyplot as plt
-from deepface import DeepFace
+# Сортировка кортежей
 
-faces = RetinaFace.extract_faces(img_path = "7.jpeg", align = True)
-for face in faces:
-  plt.imshow(face)
-  plt.show()
+# С помощью встроенной функции sorted() (не путать со списочным методом sort()) мы можем сортировать значения в кортежах.
+not_sorted_tuple = (34, 1, 8, 67, 5, 9, 0, 23)
+print(not_sorted_tuple)
 
-obj = DeepFace.verify("4.jpeg", "7.jpeg"
-          , model_name = 'ArcFace', detector_backend = 'retinaface')
-print(obj["verified"])
+sorted_tuple = tuple(sorted(not_sorted_tuple))
+print(sorted_tuple)
+# Обратите внимание, что функция sorted() возвращает список, но с помощью функции tuple() мы приводим результат сортировки к кортежу.
+# Для сортировки кортежа можно воспользоваться явным преобразованием в список и использовать метод sort():
+not_sorted_tuple = ('cc', 'aa', 'dd', 'bb')
+tmp = list(not_sorted_tuple)
+tmp.sort()
+
+sorted_tuple = tuple(tmp)
+print(sorted_tuple)
+
+# Строку можно преобразовать в кортеж с помощью функции tuple().
+letters = 'abcdefghijkl'
+tpl = tuple(letters)
+print(tpl)
+
+# Если вам нужно преобразовать строку в кортеж но не поэлементно, а целиком как 1 элемент используйте (,)
+letters = 'abcdefghijkl'
+tpl = (letters,)
+print(tpl)
