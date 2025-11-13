a606cea0784f7618b85db48afaced810e120ffab
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-14 03:57:54 +0400

    13.11 Основы работы с кортежами. Часть 2. Задача 1.

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-# Сортировка кортежей
+numbers = (2, 3, 5, 7, -11, 13, 17, 19, 23, 29, 31, -6, 41, 43, 47, 53, 59, 61, -96, 71, 1000, -1)
 
-# С помощью встроенной функции sorted() (не путать со списочным методом sort()) мы можем сортировать значения в кортежах.
-not_sorted_tuple = (34, 1, 8, 67, 5, 9, 0, 23)
-print(not_sorted_tuple)
+count = 1
+for i in numbers:
+    count *= i
 
-sorted_tuple = tuple(sorted(not_sorted_tuple))
-print(sorted_tuple)
-# Обратите внимание, что функция sorted() возвращает список, но с помощью функции tuple() мы приводим результат сортировки к кортежу.
-# Для сортировки кортежа можно воспользоваться явным преобразованием в список и использовать метод sort():
-not_sorted_tuple = ('cc', 'aa', 'dd', 'bb')
-tmp = list(not_sorted_tuple)
-tmp.sort()
-
-sorted_tuple = tuple(tmp)
-print(sorted_tuple)
-
-# Строку можно преобразовать в кортеж с помощью функции tuple().
-letters = 'abcdefghijkl'
-tpl = tuple(letters)
-print(tpl)
-
-# Если вам нужно преобразовать строку в кортеж но не поэлементно, а целиком как 1 элемент используйте (,)
-letters = 'abcdefghijkl'
-tpl = (letters,)
-print(tpl)
+print(count)
