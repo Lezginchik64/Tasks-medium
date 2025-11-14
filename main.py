34dfb11ac04a98795fc68a2942f26498aa6b4e11
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-15 01:30:49 +0400

    14.11 Конкурсный отбор

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-a, b, c = int(input()), int(input()), int(input())
-x = round(- b / (2 * a), 2)
-y = round(a * x ** 2 + b * x + c, 2)
-print((x, y,))
+n = int(input())
+students = [tuple(input().split()) for _ in range(n)]
+for res in students:
+    print(*res)
+print()
+for name, grade in students:
+    if int(grade) > 3:
+        print(name, grade)
