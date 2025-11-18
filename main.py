a2af80503f31c06f103091d4c3715b65d9aaa34d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-19 02:22:45 +0400

    18.11 8.4 Основы работы с множествами. Задача "Количество различных символов"

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-fruits = {'apple', 'banana', 'cherry', 'avocado', 'pineapple', 'apricot', 'banana', 'avocado', 'grapefruit'}
-sorted_fruits = sorted(fruits, reverse=True)
-print(*sorted_fruits, sep="\n")
+a = set([i for i in input()])
+print(len(a))
