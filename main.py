4e381a11a5f66864c7613a249ed5e791c182c3e8
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-20 00:33:44 +0400

    19.11 Озвучка текста на Python. (не курс)

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-a = set(input() + input())
-b = set('0123456789')
-print("YES" if a == b else "NO")
+from gtts import gTTS
+from playsound import playsound
+
+text = "Привет пока"
+tts = gTTS(text=text, lang='ru', slow=False)
+tts.save("test.mp3")
+playsound("test.mp3")
