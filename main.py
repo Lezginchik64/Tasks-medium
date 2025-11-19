b25878d4a5636f33113b29dfe0d7915a0ec2495e
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-20 02:41:32 +0400

    19.11 Методы множеств. Часть 1. Задача "Уникальные символы 1"

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-a, b, c = [set(i) for i in input().split()]
-print("YES" if a == b == c else "NO")
+# 1
+a = [input().lower() for i in range(int(input()))]
+for i in a:
+    print(len(set(i)))
+
+# 2
+for i in range(int(input())):
+    print(len(set(input().lower())))
