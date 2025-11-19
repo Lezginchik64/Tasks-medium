f60bc1c6a4f9c37fd80e5cdbd1de05552ae9851b
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-20 00:42:24 +0400

    19.11 Проверяем надёжность пароля с помощью Python. (не курс)

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from gtts import gTTS
-from playsound import playsound
+import re
 
-text = "Привет пока"
-tts = gTTS(text=text, lang='ru', slow=False)
-tts.save("test.mp3")
-playsound("test.mp3")
+def check_password_strength(password):
+    length = len(password) >= 8
+    upper = re.search(r"[A-Z]", password)
+    lower = re.search(r"[a-z]", password)
+    digit = re.search(r"\d", password)
+    special = re.search(r"[@$!%*?&]", password)
+
+    if all([length, upper, lower, digit, special]):
+        return "✅ Надёжный пароль"
+    else:
+        return "⚠️ Пароль слабый"
+
+print(check_password_strength("Qwerty123"))
+print(check_password_strength(input()))
