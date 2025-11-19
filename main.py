2893c8bd0fcf45c582b25a94d5b9a0714c4ea37a
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-20 02:12:23 +0400

    19.11 8.4 Основы работы с множествами. Задача "Три слова"

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-import re
-
-def check_password_strength(password):
-    length = len(password) >= 8
-    upper = re.search(r"[A-Z]", password)
-    lower = re.search(r"[a-z]", password)
-    digit = re.search(r"\d", password)
-    special = re.search(r"[@$!%*?&]", password)
-
-    if all([length, upper, lower, digit, special]):
-        return "✅ Надёжный пароль"
-    else:
-        return "⚠️ Пароль слабый"
-
-print(check_password_strength("Qwerty123"))
-print(check_password_strength(input()))
+a, b, c = [set(i) for i in input().split()]
+print("YES" if a == b == c else "NO")
