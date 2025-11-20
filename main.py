2ba20b30ceb87f4affe012a4d7141afb22aa762d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-20 17:00:36 +0400

    20.11. Вывод календаря (не курс).

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-a = [input().lower() for _ in range(int(input()))]
-b = ''.join(a)
-print(len(set(b)))
+from calendar import *
+
+year = int(input("Enter Year: "))
+# print(calendar(year, 2, 1, 8, 2)) # Вывод календаря
+
+cal = TextCalendar(firstweekday=0)  # Monday
+
+month_num = int(input("Enter Month: "))
+print(cal.formatmonth(year, month_num))
