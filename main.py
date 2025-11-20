20eeacd8b86faf40fee5179dd1e678219903d6c7
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-20 19:32:14 +0400

    20.11. Методы множеств. Часть 1. Задача: "Встречалось ли число раньше?"

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-
-words = re.sub(r'[.,;:-?-!]', '', input().lower())
-print(len(set(words.split())))
+n = [int(i) for i in input().split()]
+s = set()
+for i in n:
+    if i in s:
+        print("YES")
+    else:
+        print("NO")
+        s.add(i)
