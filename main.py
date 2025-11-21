ddf7f5a03f09a5c941d2768377422df1a6a8b189
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-22 01:51:02 +0400

    21.11. Методы множеств. Часть 2. Задача: "Общие цифры".

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-set1, set2 = {int(x) for x in input().split()}, {int(x) for x in input().split()}
-print(* sorted(set1 - set2))
-# выводит все числа в порядке возрастания, которые есть в первой строке, но отсутствуют во второй.
+# 1
+sets = [set(input()) for i in range(int(input()))]      # делаем каждую введенную строку множеством и кладем в список sets
+general = sets[0]                       # берем первый элемент списка sets, для дальнейшего сравнения с ним
+for i in range(1, len(sets)):
+    general = general & sets[i]
+print(*sorted(general))
+
+# 2
+n = int(input())
+numbers = [input() for _ in range(n)]
+
+num_set = set(numbers[0]).intersection(*numbers)        # Метод .intersection() преобразует каждую строку в множество символов. Каждый аргумент автоматически превращается во множество.
+print(*sorted(num_set))                                     # Пересечение множества самого с собой не меняет ничего. A ∩ A = A
