a37f8c75f14dec1d7f25e0b2798e5049dad05c6b
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-21 23:52:58 +0400

    21.11. Методы множеств. Часть 2. Задача: "Числа первой строки".

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 set1, set2 = {int(x) for x in input().split()}, {int(x) for x in input().split()}
-print(* sorted(set1 & set2))
+print(* sorted(set1 - set2))
+# выводит все числа в порядке возрастания, которые есть в первой строке, но отсутствуют во второй.
