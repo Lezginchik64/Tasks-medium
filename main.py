c560f08d7f5b34beee8ca30aa86cb2b09a90028b
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-24 02:11:17 +0400

    23.11. Методы множеств. Часть 3. Задача: "Урок математики 🧠".

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 a, b, c = [set(input().split()) for _ in range(3)]
-i = a.intersection(b)                               # i - общие элементы a и b
-d = sorted(list(i.difference(c)), reverse=True, key=int)     # элементы, которых нет в i, отсортированные в обратном порядке
-if i and d:                     # key=int - Перед сортировкой преобразуй каждый элемент в число
-    print(*d)
+print(*sorted(list((a | b | c) - (a & b & c)), key=int))
+# a | b | c - cумма всех сетов
+# a & b & c - элементы, которые есть во всех 3 сетах
