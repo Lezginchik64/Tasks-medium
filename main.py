829d47a1dda25424ea45ae18e96b137b18268fc6
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-24 01:43:12 +0400

    23.11. Методы множеств. Часть 3. Задача: "Урок информатики 👨‍💻".

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-a, b = set(input()), set(input())
-print("YES" if a.issuperset(b) else "NO")
+a, b, c = [set(input().split()) for _ in range(3)]
+i = a.intersection(b)                               # i - общие элементы a и b
+d = sorted(list(i.difference(c)), reverse=True, key=int)     # элементы, которых нет в i, отсортированные в обратном порядке
+if i and d:                     # key=int - Перед сортировкой преобразуй каждый элемент в число
+    print(*d)
