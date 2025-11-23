bde0f770cc63d5978a79d0a1db6c31135a00b289
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-24 02:46:08 +0400

    23.11. Методы множеств. Часть 3. Задача: "Урок биологии 🌱".

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# 1
 a, b, c = [set(input().split()) for _ in range(3)]
-print(*sorted(list(c - (a | b)), key=int, reverse=True))
-
-# 2
-a, b, c = [set(input().split()) for _ in range(3)]
-print(*sorted(list(c - b - a), key=int, reverse=True))
+d = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'}
+print(*sorted(list(d - (a | b | c)), key=int))
