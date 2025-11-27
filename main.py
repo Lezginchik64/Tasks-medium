b9b537e94e18f5aeebed308864d9993bbae88f51
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-28 01:07:31 +0400

    27.11. Методы словарей. Задача 2.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-result = {}
-print({i : i**2 for i in range(1, 16)})
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+result = dict2.copy()
+
+for i in dict1:
+    if i in dict2:
+        dict1[i] = dict1[i] + dict2[i]
+result.update(dict1)
+
+print(result)
