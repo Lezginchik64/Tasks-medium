c1916fea16d1d4e8d1d68985682c127d838674bb
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-28 01:11:18 +0400

    27.11. Методы словарей. Задача 3.

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
-dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
-result = dict2.copy()
+text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
+result = {}
 
-for i in dict1:
-    if i in dict2:
-        dict1[i] = dict1[i] + dict2[i]
-result.update(dict1)
+for i in text:
+    result[i] = result.get(i, 0) + 1
 
 print(result)
