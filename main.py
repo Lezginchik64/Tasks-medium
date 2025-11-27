e3fe17c475e99296198f83d2eaf6d17feadb425e
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-11-28 01:55:27 +0400

    27.11. Методы словарей. Задача 4. Наиболее часто встречающееся слово строки.

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
-result = {}
-
-for i in text:
-    result[i] = result.get(i, 0) + 1
-
-print(result)
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'.split()
+d = {s.count(i): i for i in sorted(s, reverse=True)}
+print(d[max(d)])
