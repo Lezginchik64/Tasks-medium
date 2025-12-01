3925568f0f96bca3d93987ee087340de357753e3
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-01 22:16:47 +0400

    01.12. Задачи на словари. Задача "Анаграммы 2".

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+# 1
+def anagramm(word):
+    d = {}
+    for i in word.lower():
+        if i.isalpha():
+            d[i] = d.get(i, 0) + 1
+    return d
+
+print("YES" if anagramm(input()) == anagramm(input()) else "NO")
+
+
+# 2
 first = {}
-for i in input():
-    first[i] = first.get(i, 0) + 1
+for i in input().lower():
+    if i in ".,!?:;- ":
+        continue
+    else:
+        first[i] = first.get(i, 0) + 1
 
 second = {}
-for i in input():
-    second[i] = second.get(i, 0) + 1
+for i in input().lower():
+    if i in ".,!?:;- ":
+        continue
+    else:
+        second[i] = second.get(i, 0) + 1
 
-print("YES" if first == second else "NO" )      # сравниваем словари, они сравниваются по ключам и значениям
+print("YES" if first == second else "NO")
