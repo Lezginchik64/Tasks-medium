7f9b10e7bfe9f5f426ff118cc86a438beba703ee
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-01 14:07:37 +0400

    01.12. Задачи на словари. Задача "Анаграммы 1". Способ 2, с помощью словарей.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("YES" if sorted(input()) == sorted(input()) else "NO")
+first = {}
+for i in input():
+    first[i] = first.get(i, 0) + 1
+
+second = {}
+for i in input():
+    second[i] = second.get(i, 0) + 1
+
+print("YES" if first == second else "NO" )      # сравниваем словари, они сравниваются по ключам и значениям
