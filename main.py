1355945fdba0a71312613826921c253f221e847f
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-04 01:15:16 +0400

    03.12. Вложенные словари, генераторы словарей. Задача 6.

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
+numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
 
-# 1
-result = {int(key): val for key, val in [i.split(":") for i in s.split()]}
-print(result)
-
-# 2
-for i in s.split():
-    key, value = i.split(":")
-    result[int(key)] = value
+result = {key: [value for value in range(1, key + 1) if key % value == 0] for key in numbers}
+# словарь result, где ключи - элементы списка numbers, а значения – отсортированный по возрастанию список всех его делителей, начиная с 1.
 print(result)
