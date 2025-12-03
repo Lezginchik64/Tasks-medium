a63950fd4f997c5cd3eafc814c8157934b183159
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-04 01:34:15 +0400

    03.12. Вложенные словари, генераторы словарей. Задача 7.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
+words = ['hello', 'bye', 'yes', 'no', 'python', 'apple', 'maybe', 'stepik', 'beegeek']
 
-result = {key: [value for value in range(1, key + 1) if key % value == 0] for key in numbers}
-# словарь result, где ключи - элементы списка numbers, а значения – отсортированный по возрастанию список всех его делителей, начиная с 1.
+result = {key: [ord(value) for value in key] for key in words}
 print(result)
