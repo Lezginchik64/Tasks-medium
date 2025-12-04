6f185281b7395e5b23cb39276ba6394817f12d93
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-05 02:13:48 +0400

    04.12. Тема: "Модуль random" Часть 1. Задача 4. !

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import random
 
 # 1
-length = int(input())
-for _ in range(length):
-    if random.randint(0,1):    # if random.randint(0,1) - если выпадает 1, то это True и выполняется условие if, если 0, то False и выполняется else.
-        print(chr(random.randint(65, 90)), end="")
-    else:
-        print(chr(random.randint(97, 122)), end="")
-print()
+s = set()
+while len(s) < 7:
+    s.add(random.randint(1, 49))
+print(*sorted(s))
 
 # 2
-length = int(input())
-password = ''
-for _ in range(length):
-    password += [chr(random.randint(65, 90)), chr(random.randint(97, 122))][random.randint(0, 1)]
-print(password)     # выбираются 2 буквы, потом рандомно выпадает 0 или 1 - это позиция в списке, 0 - первый элемент списка (1 буква), 1 - второй элемент списка (2 буква).
+# random.sample гарантирует уникальные элементы
+s = random.sample(range(1, 50), 7)
+
+print(*sorted(s))
