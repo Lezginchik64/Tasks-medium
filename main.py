bf555834774405a39dbae053fad597d03f68feaa
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-05 01:33:10 +0400

    04.12. Тема: "Модуль random" Часть 1. Задача 2.

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 import random
 
-# 1
-n = int(input())    # количество попыток
-for _ in range(n):
-    r = random.randint(0, 1)
-    if r == 0:
-        print("Орел")
-    else:
-        print("Решка")
-
-# 2
-Coin = ["Орел", "Решка"]
 for _ in range(int(input())):
-    print(Coin[random.randint(0, 1)])
-
-
-
+    r = random.randrange(1, 7)
+    print(r)
