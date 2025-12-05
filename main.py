759cccbdac5a9a4008ca4b21919dd16cd20b0ef6
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-06 01:54:02 +0400

    05.12. Тема: "Методы модуля random" Часть 2. Задача 6.

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import random
 
 # 1
-r = list(input())
-random.shuffle(r)
-print(''.join(r))
+numbers = random.sample([i for i in range(1, 76)], 25)
+bingo = [numbers[i:i + 5] for i in range(0, 21, 5)]
+bingo[2][2] = 0
+
+for i in range(5):
+    for j in range(5):
+        print(str(bingo[i][j]).ljust(3), end=' ')
+    print()
+print()
 
 # 2
-anagram = input()
-print(''.join(random.sample(anagram, len(anagram))))
+l = [i for i in range(1, 76)]  # создаем список для заполнения матрицы
+sample = random.sample(l, 24)  # перемешиваем список, и выбираем 24 случайных уникальных числа
+matrix = [[0] * 5 for row in range(5)]  # создаем матрицу и заполняем нулями
+index = 0  # счетчик для перебора по списку sample
+
+for i in range(len(matrix)):
+    for j in range(len(matrix)):
+        if i == 2 and j == 2:  # центральное значение == 0
+            matrix[i][j] == 0
+        else:
+            matrix[i][j] = sample[index]
+            index += 1
+
+for res in matrix:
+    print(*[str(i).ljust(3) for i in res])
