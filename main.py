c034ba67632b1a1a2ef36a9d66a00b120a430f1a
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-05 14:41:56 +0400

    05.12. Тема: "Методы модуля random" Часть 2. Задача 2.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from random import *
+from random import choice, randrange
+from string import ascii_uppercase as letter
 
 
-def generate_ip():
-    return (f"{randrange(256)}.{randrange(256)}.{randrange(256)}.{randrange(256)}")
+def generate_index():
+    return (
+        f"{choice(letter)}{choice(letter)}{randrange(100)}_{randrange(100)}{choice(letter)}{choice(letter)}")
 
 
-print(generate_ip())
+print(generate_index())
