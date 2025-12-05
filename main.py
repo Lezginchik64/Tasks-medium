3531d177ea0a44d18f924949bc34f4ef37ff2ef1
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-05 15:42:02 +0400

    05.12. Тема: "Методы модуля random" Часть 2. Задача 5.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 
-s = set()
-while len(s) < 100:
-    tickets = random.randint(1000000, 9999999)  # 1.000.000 — самое маленькое 7-значное число, 9.999.999 — самое большое 7-значное число
-    s.add(tickets)
+# 1
+r = list(input())
+random.shuffle(r)
+print(''.join(r))
 
-for i in s:
-    print(i)
+# 2
+anagram = input()
+print(''.join(random.sample(anagram, len(anagram))))
