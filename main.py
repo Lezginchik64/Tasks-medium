c88575a84fea084fb21aaf1cef5a27ba4a544c07
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-05 15:34:14 +0400

    05.12. Тема: "Методы модуля random" Часть 2. Задача 4.

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from random import shuffle
+import random
 
-matrix = [[1, 2, 3, 4],
-          [5, 6, 7, 8],
-          [9, 10, 11, 12],
-          [13, 14, 15, 16]]
+s = set()
+while len(s) < 100:
+    tickets = random.randint(1000000, 9999999)  # 1.000.000 — самое маленькое 7-значное число, 9.999.999 — самое большое 7-значное число
+    s.add(tickets)
 
-flat = [x for row in matrix for x in row]       # Делаем список из всех элементов
-shuffle(flat)                                   # перемешиваем список
-
-new_matrix = [flat[i:i+4] for i in range(0, 16, 4)]    # собираем обратно в матрицу 4×4
-print(new_matrix)       # range(0, 16, 4) - индексы начала строк: 0, 4, 8, 12
-                        # flat[i:i+4]   -  flat[0:4],
-#                                          flat[4:8],
-#                                          flat[8:12],
-#                                          flat[12:16]
+for i in s:
+    print(i)
