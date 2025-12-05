ec6acf268101ab6fffff9c07150a847f98633bad
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-05 15:06:35 +0400

    05.12. Тема: "Методы модуля random" Часть 2. Задача 3.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from random import choice, randrange
-from string import ascii_uppercase as letter
+from random import shuffle
 
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16]]
 
-def generate_index():
-    return (
-        f"{choice(letter)}{choice(letter)}{randrange(100)}_{randrange(100)}{choice(letter)}{choice(letter)}")
+flat = [x for row in matrix for x in row]       # Делаем список из всех элементов
+shuffle(flat)                                   # перемешиваем список
 
-
-print(generate_index())
+new_matrix = [flat[i:i+4] for i in range(0, 16, 4)]    # собираем обратно в матрицу 4×4
+print(new_matrix)       # range(0, 16, 4) - индексы начала строк: 0, 4, 8, 12
+                        # flat[i:i+4]   -  flat[0:4],
+#                                          flat[4:8],
+#                                          flat[8:12],
+#                                          flat[12:16]
