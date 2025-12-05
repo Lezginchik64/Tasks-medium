dfed940eac67daa18545ec9428c37f94fb9ac9a9
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-05 14:21:26 +0400

    05.12. Тема: "Методы модуля random" Часть 2. Теория.

--- main.py
+++ main.py
@@ -1,13 +1,53 @@
+# -- Методы модуля random --
 import random
 
-# 1
-s = set()
-while len(s) < 7:
-    s.add(random.randint(1, 49))
-print(*sorted(s))
 
-# 2
-# random.sample гарантирует уникальные элементы
-s = random.sample(range(1, 50), 7)
+# -- Метод shuffle() --
+# Метод shuffle() принимает список в качестве обязательного аргумента и перемешивает его случайным образом.
+# Следующий код перемешивает список numbers случайным образом, а затем выводит его содержимое:
+print("Метод shuffle")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+random.shuffle(numbers)
+print(numbers)
+print()
 
-print(*sorted(s))
+
+# -- Метод choice() --
+# Метод choice() принимает список (строку, кортеж) в качестве обязательного аргумента и возвращает один случайный элемент.
+# Элементы могут повторяться!
+# Следующий код выводит по одному случайному элементу из строки 'BEEGEEK', списка [1, 2, 3, 4] и кортежа ('a', 'b', 'c', 'd'):
+print("Метод choice")
+print(random.choice('BEEGEEK'))
+print(random.choice([1, 2, 3, 4]))
+print(random.choice(('a', 'b', 'c', 'd')))
+print()
+
+
+# -- Метод sample() --
+# Метод sample() принимает два обязательных аргумента:
+# первый – коллекция (последовательность), которая поддерживает индексацию (список, строка, кортеж), второй – количество случайных элементов.
+# Возвращает список из указанного количества УНИКАЛЬНЫХ (имеющих разные индексы) случайных элементов.
+print("Метод sample")
+numbers = [2, 5, 8, 9, 12]
+print(random.sample(numbers, 1))
+print(random.sample(numbers, 2))
+print(random.sample(numbers, 3))
+print(random.sample(numbers, 5))
+print()
+# Количество случайных элементов не должно превышать длину исходного списка (строки)
+
+
+# -- Модуль string --
+# Встроенный модуль string раньше использовался для расширения стандартных возможностей (функционала) строкового типа данных str.
+# На текущий момент все функции из модуля string переехали в методы строкового типа данных str, однако в модуле string остались удобные константные строки, которые можно использовать при решении задач.
+import string
+
+print("Модуль string")
+print(string.ascii_letters)
+print(string.ascii_uppercase)
+print(string.ascii_lowercase)
+print(string.digits)
+print(string.hexdigits)
+print(string.octdigits)
+print(string.punctuation)
+print(string.printable)
