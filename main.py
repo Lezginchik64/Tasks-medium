8298d5501bdaf54cab0925e8d01c525373305731
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-08 03:15:16 +0400

    07.12. Тема: "Методы модуля random" Часть 2. Задача 9: "Генератор паролей 2 🌶️".

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import random
 from string import *
 
+# 1
 letter = ''.join((set(ascii_letters) | set(digits)) - set('lI1oO0'))
-# преобразуем в set для возможности сложения и вычитания набора символов. | - сложение
-
 
 def generate_password(length):
-    return "".join(random.sample(letter, length))
+    while True:
+        s = "".join(random.sample(letter, length))
+        if not (s.isupper() or s.islower() or s.isdigit() or s.isalpha()):
+            return s
 
 
 def generate_passwords(count, length):
     return [generate_password(length) for _ in range(count)]
 
-# def generate_passwords(count, length):
-#     passwords = []
-#     for i in range(count):
-#         password = "".join(random.sample(letter, length))
-#         passwords.append(password)
-#     return "\n".join(passwords)
+
+n, m = int(input()), int(input())
+print(*generate_passwords(n, m), sep='\n')
+
+# 2
+chars1 = [c for c in ascii_uppercase if c not in 'OI']
+chars2 = [c for c in ascii_lowercase if c not in 'ol']
+chars3 = list(digits[2:])
+chars = chars1 + chars2 + chars3
+
+
+def generate_password(length):
+    res = [random.choice(i) for i in (chars1, chars2, chars3)] + [random.choice(chars) for _ in range(3, length)]
+    # добавляем в res по одному символу из каждого списка (chars1, chars2, chars3), а оставшиеся заполняем любыми символами из общего списка chars
+    random.shuffle(res)
+    return "".join(res)
+
+
+def generate_passwords(count, length):
+    res = set()
+    while len(res) < count:
+        res.add(generate_password(length))
+    return list(res)
 
 
 n, m = int(input()), int(input())
