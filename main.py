7681bb9e4a41d20112e4ca47253b3bcbc2b7b088
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-08 02:29:14 +0400

    07.12. Тема: "Методы модуля random" Часть 2. Задача 8: "Генератор паролей 1".

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import random
+from string import *
 
-friends = [input() for i in range(int(input()))]
-random.shuffle(friends)
+letter = ''.join((set(ascii_letters) | set(digits)) - set('lI1oO0'))
+# преобразуем в set для возможности сложения и вычитания набора символов. | - сложение
 
-for i in range(len(friends)):
-    print(f"{friends[i]} - {friends[i - 1]}")
+
+def generate_password(length):
+    return "".join(random.sample(letter, length))
+
+
+def generate_passwords(count, length):
+    return [generate_password(length) for _ in range(count)]
+
+# def generate_passwords(count, length):
+#     passwords = []
+#     for i in range(count):
+#         password = "".join(random.sample(letter, length))
+#         passwords.append(password)
+#     return "\n".join(passwords)
+
+
+n, m = int(input()), int(input())
+print(*generate_passwords(n, m), sep='\n')
