7622565c658eb1a3d7d863935da24ed8337e52a3
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-08 00:56:19 +0400

    07.12. Тема: "Методы модуля random" Часть 2. Задача 7.

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import requests
+import random
 
+friends = [input() for i in range(int(input()))]
+random.shuffle(friends)
 
-def get_weather(lat, lon):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
-    return requests.get(url).json()['current_weather']
-
-
-saratov_coords = (51.53, 46)
-coords = (51.60, 46.02)
-weather = get_weather(*saratov_coords)
-weather2 = get_weather(*coords)
-print(f"Температура в Саратове: {weather['temperature']}°C, Ветер: {weather['windspeed']} км/ч")
-print(f"Температура: {weather2['temperature']}°C, Ветер: {weather2['windspeed']} км/ч")
+for i in range(len(friends)):
+    print(f"{friends[i]} - {friends[i - 1]}")
