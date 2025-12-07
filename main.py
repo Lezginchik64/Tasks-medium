8582350c3458461d30c0c8a1982b2f43ad91e5dc
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-08 00:36:45 +0400

    07.12. Парсер погоды.

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
-from captcha.image import ImageCaptcha
-from PIL import Image
-import random
-import string
+import requests
 
 
-def generate_captcha_text(length=10):
-    return ''.join(
-        random.choices(string.ascii_letters + string.digits, k=length)
-    )
+def get_weather(lat, lon):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    return requests.get(url).json()['current_weather']
 
-def generate_captcha_image(captcha_text, image_width=300):
-    image = ImageCaptcha(image_width)
-    image_file = f"{captcha_text}.png"
-    image.write(captcha_text, image_file)
-    return image_file
 
-captcha_text = generate_captcha_text()
-image_file = generate_captcha_image(captcha_text)
-print("Generated CAPTCHA")
-Image.open(image_file).show()
-
-attempts = 3
-for attempt in range(attempts):
-    user_input = input(f"\nПопытка {attempt + 1}/{attempts}: Введите текст: ")
-
-    if user_input.lower() == captcha_text.lower():
-        print(f"✅ Верно! Код: {captcha_text.lower()}")
-        break
-    else:
-        print(f"❌ Неверно. Осталось попыток: {attempts - attempt - 1}")
-else:
-    print(f"💀 Все попытки исчерпаны. Доступ заблокирован.")
-
+saratov_coords = (51.53, 46)
+coords = (51.60, 46.02)
+weather = get_weather(*saratov_coords)
+weather2 = get_weather(*coords)
+print(f"Температура в Саратове: {weather['temperature']}°C, Ветер: {weather['windspeed']} км/ч")
+print(f"Температура: {weather2['temperature']}°C, Ветер: {weather2['windspeed']} км/ч")
