e224a87405d1c073ef274c67748ebbfcfb46483f
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-09 03:16:57 +0400

    08.12. Тема: 12.3 Метод Монте-Карло и Bogosort. Задача 1.

--- main.py
+++ main.py
@@ -1,43 +1,13 @@
 import random
-from string import *
 
-# 1
-letter = ''.join((set(ascii_letters) | set(digits)) - set('lI1oO0'))
+n = 10**6
+k = 0
+s0 = 16
+for _ in range(n):
+    x = random.uniform(-2, 2)
+    y = random.uniform(-2, 2)
 
-def generate_password(length):
-    while True:
-        s = "".join(random.sample(letter, length))
-        if not (s.isupper() or s.islower() or s.isdigit() or s.isalpha()):
-            return s
+    if x**3 + y**4 + 2 >= 0 and 3*x + y**2 <= 2:
+        k += 1
 
-
-def generate_passwords(count, length):
-    return [generate_password(length) for _ in range(count)]
-
-
-n, m = int(input()), int(input())
-print(*generate_passwords(n, m), sep='\n')
-
-# 2
-chars1 = [c for c in ascii_uppercase if c not in 'OI']
-chars2 = [c for c in ascii_lowercase if c not in 'ol']
-chars3 = list(digits[2:])
-chars = chars1 + chars2 + chars3
-
-
-def generate_password(length):
-    res = [random.choice(i) for i in (chars1, chars2, chars3)] + [random.choice(chars) for _ in range(3, length)]
-    # добавляем в res по одному символу из каждого списка (chars1, chars2, chars3), а оставшиеся заполняем любыми символами из общего списка chars
-    random.shuffle(res)
-    return "".join(res)
-
-
-def generate_passwords(count, length):
-    res = set()
-    while len(res) < count:
-        res.add(generate_password(length))
-    return list(res)
-
-
-n, m = int(input()), int(input())
-print(*generate_passwords(n, m), sep='\n')
+print((k/n)*s0)
