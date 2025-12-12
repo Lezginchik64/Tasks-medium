8a016000fd0d6aab5a31cd83a32ae7c971d3403a
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-13 02:41:15 +0400

    12.12. Тема:"Модуль fractions". Задача 6 "Сумма дробей 2".

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fractions import Fraction
+from math import factorial as f
 
+# 1
+print(sum([Fraction(1, f(i)) for i in range(1, int(input()) + 1)]))
+
+# 2
 res = 0
 for i in range(1, int(input()) + 1):
-    res += Fraction(1, i ** 2)
+    res += Fraction(1, f(i))
 print(res)
