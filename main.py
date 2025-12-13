683d472beaaa01bd88c3bbca6b7136f2da5afa75
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-14 01:21:19 +0400

    13.12. Тема:"Модуль fractions". Задача 7 "Юный математик 🤓🌶️".

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from fractions import Fraction
-from math import factorial as f
+from fractions import Fraction as F
+from math import gcd
 
-# 1
-print(sum([Fraction(1, f(i)) for i in range(1, int(input()) + 1)]))
-
-# 2
-res = 0
-for i in range(1, int(input()) + 1):
-    res += Fraction(1, f(i))
-print(res)
+n = int(input())
+a = n // 2  # числитель
+b = n - a  # знаменатель  a + b = n, поэтому b = n - a
+while gcd(a, b) != 1:       # gcd - наибольший общий делитель
+    a -= 1
+    b += 1
+print(F(a, b))
