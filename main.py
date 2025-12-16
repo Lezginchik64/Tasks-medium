4c1384d2c189ea4e06a53f98ebfd990cd0262c47
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-12-17 02:55:49 +0400

    16.12. Тема: "Функции с переменным количеством аргументов". Задача 3.

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-def sq_sum(*args):
-    return sum([num ** 2 for num in args])
+# 1
+def mean(*args):
+    n = [i for i in args if type(i) == int or type(i) == float]
+    return sum(n) / len(n) if n else 0.0
 
 
-print(sq_sum())
-print(sq_sum(2))
-print(sq_sum(1.5, 2.5))
-print(sq_sum(1, 2, 3))
-print(sq_sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+# 2
+def mean(*args):
+    args = [float(i) for i in args if type(i) in (int, float)]
+    if len(args) > 0:
+        return sum(args) / len(args)
+    else:
+        return 0.0
+
+
+print(mean())
+print(mean(7))
+print(mean(1.5, True, ['stepik'], 'beegeek', 2.5, (1, 2)))
+print(mean(True, ['stepik'], 'beegeek', (1, 2)))
+print(mean(-1, 2, 3, 10, ('5')))
+print(mean(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
