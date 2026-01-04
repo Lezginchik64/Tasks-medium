3571610867fc3bfaf6896750f64bc7cf6a743f51
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-05 02:37:05 +0400

    04.01 Тема: "Анонимные функции. Часть 2". Задача 5.

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-is_num = lambda x: x.removeprefix('-').replace('.', '', 1).isdigit()
-print(is_num('10.34ab'))        # можно удалять единственный префикс методом строк removeprefix
-print(is_num('10.45'))
-print(is_num('-18'))
-print(is_num('-34.67'))
-print(is_num('987'))
-print(is_num('abcd'))
-print(is_num('123.122.12'))
-print(is_num('-123.122'))
-print(is_num('--13.2'))
-print(is_num('1-1'))
+words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able',
+         'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound',
+         'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday',
+         'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry',
+         'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+
+res = sorted(list(filter(lambda word: len(word) == 6, words)))
+print(*res)
