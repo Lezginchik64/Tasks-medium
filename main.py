daee6ecc343b1a63e90f6b19856a5879da6230c6
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-05 02:33:59 +0400

    04.01 Тема: "Анонимные функции. Часть 2". Задача 4.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-is_non_negative_num = lambda x: x.replace('.', '', 1).isdigit()        # isdigit - будет всегда возвращать False если число отрицательное.
-print(is_non_negative_num('10.34ab'))                           # x.replace('.', '', 1) - заменяет точку на пустое один раз
-print(is_non_negative_num('10.45'))
-print(is_non_negative_num('-18'))
-print(is_non_negative_num('-34.67'))
-print(is_non_negative_num('987'))
-print(is_non_negative_num('abcd'))
-print(is_non_negative_num('123.122.12'))
-print(is_non_negative_num('123.122'))
+is_num = lambda x: x.removeprefix('-').replace('.', '', 1).isdigit()
+print(is_num('10.34ab'))        # можно удалять единственный префикс методом строк removeprefix
+print(is_num('10.45'))
+print(is_num('-18'))
+print(is_num('-34.67'))
+print(is_num('987'))
+print(is_num('abcd'))
+print(is_num('123.122.12'))
+print(is_num('-123.122'))
+print(is_num('--13.2'))
+print(is_num('1-1'))
