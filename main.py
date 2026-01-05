de3c2622d8deec43c0bbf0503093a5554daa69e7
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-06 00:00:55 +0400

    05.01 Тема: "Анонимные функции. Часть 2". Задача 8.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-data = [(19542209, 'New York'), (4887871, 'Alabama'), (1420491, 'Hawaii'), (626299, 'Vermont'),
-        (1805832, 'West Virginia'), (39865590, 'California'), (11799448, 'Ohio'), (10711908, 'Georgia'),
-        (10077331, 'Michigan'), (10439388, 'Virginia'), (7705281, 'Washington'), (7151502, 'Arizona'),
-        (7029917, 'Massachusetts'), (6910840, 'Tennessee')]
+data = ['год', 'человек', 'время', 'дело', 'жизнь', 'день', 'рука', 'раз', 'работа', 'слово', 'место', 'лицо', 'друг',
+        'глаз', 'вопрос', 'дом', 'сторона', 'страна', 'мир', 'случай', 'голова', 'ребенок', 'сила', 'конец', 'вид',
+        'система', 'часть', 'город', 'отношение', 'женщина', 'деньги']
 
 # 1
-for pop, city in sorted(data, key=lambda x: x[1][-1], reverse=True):
-    print(f'{city}: {pop}')
+print(*sorted(data, key=lambda x: (len(x), x)))
 
 # 2
-data = sorted(data, key=lambda word: word[1][-1], reverse=True)
-res = {val: key for key, val in data}
-for i in res:
-    print(f'{i}: {res[i]}')
+print(*sorted(sorted(data), key=lambda x: len(x)))
