45e5f9777f4f41dd39b499e636a05c753637f011
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-11 23:54:15 +0400

    11.01 Тема: "Встроенные функции any(), all(), zip(), enumerate()". Задача 2.

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def ignore_command(command):
-    ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
-    return any(map(lambda x: x in command, ignore))
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
 
-print(ignore_command('get ip'))
-
-# def ignore_command(command):
-#     ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
-#
-#     for word in ignore:
-#         if word in command:
-#             return True
-#     return False
+for a, b, c in zip(population, countries, capitals):
+    print(f'{c} is the capital of {b}, population equal {a} people.')
