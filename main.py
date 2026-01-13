cb323360beb1014bc7c02537027827c8e1b3210d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-13 23:21:26 +0400

    13.01 Тема: "Встроенные функции any(), all(), zip(), enumerate()". Задача 4 "Корректный IP-адрес".

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-# 1
-abscissas, ordinates, applicates = (map(float, input().split()) for _ in range(3))
-print(all(map(lambda x: x[0] ** 2 + x[1] ** 2 + x[2] ** 2 <= 4, zip(abscissas, ordinates, applicates))))
-
-# 2
-abscissas = [float(i) for i in input().split()]
-ordinates = [float(i) for i in input().split()]
-applicates = [float(i) for i in input().split()]
-
-num = list(zip(abscissas, ordinates, applicates))
-res = all(x ** 2 + y ** 2 + z ** 2 <= 4 for x, y, z in num)
-print(res)
+print(all(map(lambda x: x.isdigit() and 0 <= int(x) <= 255, input().split("."))))
