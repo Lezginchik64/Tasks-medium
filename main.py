d31aa3fa2a23b51a478abd12fca045739d8fb309
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-15 02:52:48 +0400

    14.01 Тема: "Встроенные функции any(), all(), zip(), enumerate()". Задача 7 "Отличники".

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-password = input()
-print("YES" if all([any(i.isupper() for i in password), any(i.islower() for i in password),
-                    any(i.isdigit() for i in password), len(password) >= 7]) else 'NO')
+result = []
+for _ in range(int(input())):
+    res = []
+    for _ in range(int(input())):
+        res.append(any(map(lambda x: x == "5", input().split())))   # есть ли ученики с 5 вообще
+    result.append(any(res))     # в каждом ли классе есть ученики с 5
+print("YES" if all(result) else "NO")   # если во все классах есть ученики с 5, выводим YES
