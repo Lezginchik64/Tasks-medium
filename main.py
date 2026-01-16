8e164a422f25671272fbbff39fb1a10a538d325e
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-16 23:34:06 +0400

    16.01 Тема: "Работа с текстовыми файлами. Часть 1". Задача 3 "Случайная строка 🎲".

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-file = open(input())
-print(file.readlines()[-3])
+import random
+
+file = open('asd.txt')
+lines = [line.strip() for line in file.readlines() if line.strip()]
+print(random.choice(lines))
+
 file.close()
