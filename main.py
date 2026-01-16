5daa78fd95fd47489db6bd65717f906e633451d0
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-17 01:18:59 +0400

    16.01 Тема: "Работа с текстовыми файлами. Часть 1". Задача 6 "Общая стоимость 💰".

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-file = open(r'/Users/lezginchik/Downloads/nums.txt')
+file = open(r'/Users/lezginchik/Downloads/prices.txt')
 
+lines = [line.split('\t') for line in file]
 # 1
-lines = file.read().split()
-print(sum(map(int, lines)))
-
+res = sum(map(lambda x: int(x[1]) * int(x[2]), lines))
 # 2
-lines = [i.strip() for i in file if i.strip()]
-print(sum(map(int, lines)))
+res2 = sum([int(i[1]) * int(i[2]) for i in lines])
+
+print(res)
+print(res2)
 
 file.close()
