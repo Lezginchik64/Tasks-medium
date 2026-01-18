be0d959fec7bba09ca0b06fbb9ba5ab2ad61aa82
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-19 01:30:19 +0400

    18.01 Тема: "Работа с текстовыми файлами. Часть 2". Задача 4 "Сумма чисел в строках️"

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 # 1
 with open("asd.txt") as file:
-    max_len, res = 0, []
     for line in file:
-        if len(line) > max_len:
-            max_len, res = len(line), [line]    # res = [line] создает новый список, содержащий только одну строку line.
-        elif len(line) == max_len:
-            res.append(line)
-print(''.join(res))
+        print(sum(map(int, line.split())))
 
 # 2
 with open("asd.txt") as file:
-    lines = [line.strip() for line in file.readlines()]
-    maxline = len(max(lines, key=len))
-    print(*filter(lambda x: len(x) == maxline, lines), sep="\n")
-
-# 3
-with open("lines.txt") as file:
-    mline = len(max(file.readlines(), key=len))
-    file.seek(0)  # курсор на начало
-    for line in file.readlines():
-        if len(line) == mline:
-            print(line.strip())
+    for line in file:
+        line = [int(i) for i in line.strip().split()]
+        print(sum(line))
