3d08d018eb269c92e3d13d80ad911ac5381de17a
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-19 00:38:40 +0400

    18.01 Тема: "Работа с текстовыми файлами. Часть 2". Задача 2 "Обратный порядок 🔄"

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-with open('text.txt', encoding='utf-8') as file:
-    print(file.read()[::-1])
+# 1
+with open("asd.txt", encoding='utf-8') as file:
+    print(*file.readlines()[::-1], sep=" ")
+
+# 2
+with open("asd.txt") as file:
+    for line in file.readlines()[::-1]:
+        print(line.strip())
