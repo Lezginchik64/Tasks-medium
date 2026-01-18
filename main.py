d272db70cc75b77f1263c20cd1427d695ddbd043
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-19 01:19:30 +0400

    18.01 Тема: "Работа с текстовыми файлами. Часть 2". Задача 3 "Длинные строки ↔️"

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 # 1
-with open("asd.txt", encoding='utf-8') as file:
-    print(*file.readlines()[::-1], sep=" ")
+with open("asd.txt") as file:
+    max_len, res = 0, []
+    for line in file:
+        if len(line) > max_len:
+            max_len, res = len(line), [line]    # res = [line] создает новый список, содержащий только одну строку line.
+        elif len(line) == max_len:
+            res.append(line)
+print(''.join(res))
 
 # 2
 with open("asd.txt") as file:
-    for line in file.readlines()[::-1]:
-        print(line.strip())
+    lines = [line.strip() for line in file.readlines()]
+    maxline = len(max(lines, key=len))
+    print(*filter(lambda x: len(x) == maxline, lines), sep="\n")
+
+# 3
+with open("lines.txt") as file:
+    mline = len(max(file.readlines(), key=len))
+    file.seek(0)  # курсор на начало
+    for line in file.readlines():
+        if len(line) == mline:
+            print(line.strip())
