ed3cf394c1cefd2a9e20eb1f1e24e61df956cc7d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-20 02:00:56 +0400

    19.01 Тема: "Работа с текстовыми файлами. Часть 2". Задача 6 "Статистика по файлу 📊"

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # 1
-with open("asd.txt") as file:
-    row = ''.join(i if i.isdigit() else " " for i in file.read())
-    print(sum(map(int, row.split())))
+with open('file.txt') as f:
+    txt = f.read()
+    print('Input file contains:')
+    print(sum(map(str.isalpha, txt)), 'letters')  # letters
+    print(len(txt.split()), 'words')  # words
+    print(txt.count('\n') + 1, 'lines')  # lines
 
 # 2
-with open("asd.txt") as file:
-    lines = file.read()
-    for i in filter(lambda x: not x.isdigit(), lines):
-        lines = lines.replace(i, " ")
-    print(sum(map(int, lines.split())))
+with open("file.txt") as file:
+    letters, words, lines = 0, 0, 0
+    for line in file:
+        lines += 1  # lines
+        chars = list(line.strip())
+        letters_list = list(filter(lambda x: x.isalpha(), chars))
+        letters += len(letters_list)  # letters
+        words += len(line.strip().split())  # words
+
+print(f"Input file contains:\n{letters} letters\n{words} words\n{lines} lines ")
