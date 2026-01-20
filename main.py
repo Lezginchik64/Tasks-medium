cb58741c052a38f84991475225689a66eeabaf07
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-21 01:52:38 +0400

    20.01 Тема: "Работа с текстовыми файлами. Часть 2". Задача 7 "Random name and surname 🎲" !

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-# 1
-with open('file.txt') as f:
-    txt = f.read()
-    print('Input file contains:')
-    print(sum(map(str.isalpha, txt)), 'letters')  # letters
-    print(len(txt.split()), 'words')  # words
-    print(txt.count('\n') + 1, 'lines')  # lines
+import random
 
-# 2
-with open("file.txt") as file:
-    letters, words, lines = 0, 0, 0
-    for line in file:
-        lines += 1  # lines
-        chars = list(line.strip())
-        letters_list = list(filter(lambda x: x.isalpha(), chars))
-        letters += len(letters_list)  # letters
-        words += len(line.strip().split())  # words
-
-print(f"Input file contains:\n{letters} letters\n{words} words\n{lines} lines ")
+with open('first_names.txt') as names, open('last_names.txt') as surnames:
+    name = [line.strip() for line in names]
+    surname = [line.strip() for line in surnames]
+    print(random.choice(name), random.choice(surname))
+    print(random.choice(name), random.choice(surname))
+    print(random.choice(name), random.choice(surname))
