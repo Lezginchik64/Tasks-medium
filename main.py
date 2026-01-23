73f9768c79ec7ed9f9573683da0f23b7ddc5f0c8
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2026-01-23 13:46:20 +0400

    23.01 Тема: "Работа с текстовыми файлами. Часть 3". Задача 3 "Нумерация строк 🔢".

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
+# 1
+with open('output.txt', 'w', encoding='utf-8') as f, open('input.txt', 'r', encoding='utf-8') as line:
+    for ind, val in enumerate(line):
+        f.writelines(f"{ind + 1}) {val}")
 
-with open('output.txt', 'w', encoding='utf-8') as f:
-    for i in range(25):
-        f.write(str(random.randrange(111, 778)) + '\n')
+# 2
+with open('output.txt', 'w', encoding='utf-8') as f, open('input.txt', 'r', encoding='utf-8') as line:
+    for ind, val in enumerate(line, start=1):
+        print(f"{ind + 1}) {val}", end="", file=f)
